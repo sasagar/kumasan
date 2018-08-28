e1683c5d4611cdd69e2a31b855e068aef2453a75
SASAGAWA, Kiyoshi <sasagawa.ki@asiist.com>
2018-08-28 10:15:47 +0900

    handler形式に修正。scriptにzip/upload/deployを追加

--- index.js
+++ index.js
@@ -20,42 +20,78 @@ const url = "https://spla2.yuu26.com/coop/schedule";
 let response;
 const ChannelName = "クマサン商会";
 
-https
-  .get(url, function(res) {
-    let body = "";
-    res.setEncoding("utf8");
-    res.on("data", function(chunk) {
-      body += chunk;
+exports.handler = (event, context) => {
+  https
+    .get(url, function(res) {
+      let body = "";
+      res.setEncoding("utf8");
+      res.on("data", function(chunk) {
+        body += chunk;
+      });
+      res.on("end", function(chunk) {
+        // body の値を json としてパース
+        res = JSON.parse(body);
+        response = res.result;
+      });
+    })
+    .on("error", function(e) {
+      console.log(e.message);
     });
-    res.on("end", function(chunk) {
-      // body の値を json としてパース
-      res = JSON.parse(body);
-      response = res.result;
-    });
-  })
-  .on("error", function(e) {
-    console.log(e.message);
+
+  bot.on("ready", () => {
+    // bot が準備できたら呼び出される
+    //console.log("Ready!");
+
+    const now = new Date();
+    const start = new Date(response[0].start);
+    const end = new Date(response[0].end);
+    const nextStart = new Date(response[1].start);
+    const nextEnd = new Date(response[1].end);
+    let msg;
+    // 分岐条件はLambdaの起動時間・多少のブレを考慮して幅を持たせている感じ
+    if (
+      // 開始前1時間 (startが今から58分後～62分後)
+      now.getTime() + 62 * 60 * 1000 >= start.getTime() &&
+      now.getTime() + 58 * 60 * 1000 <= start.getTime()
+    ) {
+      console.log("開始1時間前処理");
+      msg = "まもなく始まるシフトのお知らせです。";
+      sendMessage(response[0], msg);
+    } else if (
+      // 終了前2時間 (endが今から118分後～122分後)
+      now.getTime() + 122 * 60 * 1000 >= end.getTime() &&
+      now.getTime() + 118 * 60 * 1000 >= end.getTime()
+    ) {
+      console.log("終了2時間前処理");
+      msg = "まもなくシフトが終了します。";
+      sendMessage(response[0], msg);
+    } else if (
+      // 終了時 (endが今から2分前～2分後)
+      now.getTime() - 2 * 60 * 1000 >= end.getTime() &&
+      now.getTime() + 2 * 60 * 1000 <= end.getTime()
+    ) {
+      console.log("終了時処理");
+      msg = "シフトが終了しましたので、次のシフトのお知らせです。";
+      sendMessage(response[1], msg);
+    } else {
+      console.log("通知なし");
+      bot.disconnect();
+      context.succeed("正常終了");
+    }
   });
 
-bot.on("ready", () => {
-  // bot が準備できたら呼び出される
-  //console.log("Ready!");
+  // Discord に接続します。
+  bot.connect();
 
-  const now = new Date();
-  const start = new Date(response[0].start);
-  if (
-    (now.getTime() + 65 * 60 * 1000 >= start.getTime() &&
-      now.getTime() + 5 * 60 * 1000 <= start.getTime()) ||
-    (now.getTime() + 11.9 * 60 * 60 * 1000 <= start.getTime() &&
-      now.getTime() + 12.5 * 60 * 60 * 1000 <= start.getTime())
-  ) {
-    // 全サーバに送り出す処理
+  // exitまでの大まかな処理
+  const sendMessage = res => {
     const promise = new Promise((resolve, reject) => {
       bot.guilds.forEach(guild => {
         guild.channels.forEach(channel => {
+          console.log(guild.id);
           if (channel.name === ChannelName) {
             // この辺りでresolve必要っぽそう
-            const content = generateShiftFormat(response[0]);
+            const content = generateShiftFormat(res);
             resolve(bot.createMessage(channel.id, content));
           }
         });
@@ -66,114 +102,102 @@ bot.on("ready", () => {
     promise
       .then(() => {
         bot.disconnect();
+        context.succeed("正常終了");
         process.exit(0);
       })
       .catch(e => {
-        console.log(e);
+        context.fail(e);
         process.exit(1);
       });
-  } else {
-    bot.disconnect();
-    process.exit(0);
-  }
-});
+  };
 
-// Discord に接続します。
-bot.connect();
+  // メッセージを作る
+  const generateShiftFormat = (data, msg = "") => {
+    const now = new Date();
+    const start = new Date(data.start);
+    const end = new Date(data.end);
+    const start_f = datetostr(start, "MM/DD(WW) hh:mm", false);
+    const end_f = datetostr(end, "MM/DD(WW) hh:mm", false);
 
-// メッセージを作る
-const generateShiftFormat = data => {
-  const now = new Date();
-  const start = new Date(data.start);
-  const end = new Date(data.end);
-  const start_f = datetostr(start, "MM/DD(WW) hh:mm", false);
-  const end_f = datetostr(end, "MM/DD(WW) hh:mm", false);
-  let msg;
-  if (now.getTime() >= start.getTime()) {
-    msg = "ただいま募集中のシフトのお知らせです";
-  } else if (now.getTime() + 60 * 60 * 1000 > start.getTime()) {
-    msg = "まもなく始まるシフトのお知らせです";
-  } else {
-    msg = "次のシフトのお知らせです";
-  }
-  const embeds = {
-    color: parseInt("e55833", 16),
-    image: {
-      url: data.stage.image
-    },
-    fields: [
-      {
-        name: "シフト",
-        value: start_f + " ～ " + end_f
+    const embeds = {
+      color: parseInt("e55833", 16),
+      image: {
+        url: data.stage.image
       },
-      {
-        name: "ステージ",
-        value: data.stage.name
-      },
-      {
-        name: "支給ブキ",
-        value:
-          data.weapons[0].name +
-          "・" +
-          data.weapons[2].name +
-          "・" +
-          data.weapons[1].name +
-          "・" +
-          data.weapons[3].name
-      }
-    ]
-  };
-  const content = {
-    content: msg,
-    embed: embeds
-  };
+      fields: [
+        {
+          name: "シフト",
+          value: start_f + " ～ " + end_f
+        },
+        {
+          name: "ステージ",
+          value: data.stage.name
+        },
+        {
+          name: "支給ブキ",
+          value:
+            data.weapons[0].name +
+            "・" +
+            data.weapons[2].name +
+            "・" +
+            data.weapons[1].name +
+            "・" +
+            data.weapons[3].name
+        }
+      ]
+    };
+    const content = {
+      content: msg,
+      embed: embeds
+    };
 
-  return content;
-};
-
-// 日付のフォーマット
-const datetostr = (date, format, is12hours) => {
-  let wdays = ["日", "月", "火", "水", "木", "金", "土"];
-  if (!format) {
-    format = "YYYY/MM/DD(WW) hh:mm:dd";
-  }
-  let year = date.getFullYear();
-  let month = date.getMonth() + 1;
-  let day = date.getDate();
-  let weekday = wdays[date.getDay()];
-  let hours = date.getHours();
-  let minutes = date.getMinutes();
-  let secounds = date.getSeconds();
-
-  let ampm = hours < 12 ? "AM" : "PM";
-  if (is12hours) {
-    hours = hours % 12;
-    hours = hours != 0 ? hours : 12; // 0時は12時と表示する
-  }
-
-  let replaceStrArray = {
-    YYYY: year,
-    Y: year,
-    MM: ("0" + month).slice(-2),
-    M: month,
-    DD: ("0" + day).slice(-2),
-    D: day,
-    WW: weekday,
-    hh: ("0" + hours).slice(-2),
-    h: hours,
-    mm: ("0" + minutes).slice(-2),
-    m: minutes,
-    ss: ("0" + secounds).slice(-2),
-    s: secounds,
-    AP: ampm
+    return content;
   };
 
-  let replaceStr = "(" + Object.keys(replaceStrArray).join("|") + ")";
-  let regex = new RegExp(replaceStr, "g");
+  // 日付のフォーマット
+  const datetostr = (date, format, is12hours) => {
+    let wdays = ["日", "月", "火", "水", "木", "金", "土"];
+    if (!format) {
+      format = "YYYY/MM/DD(WW) hh:mm:dd";
+    }
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
+    let weekday = wdays[date.getDay()];
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let secounds = date.getSeconds();
 
-  ret = format.replace(regex, function(str) {
-    return replaceStrArray[str];
-  });
+    let ampm = hours < 12 ? "AM" : "PM";
+    if (is12hours) {
+      hours = hours % 12;
+      hours = hours != 0 ? hours : 12; // 0時は12時と表示する
+    }
 
-  return ret;
+    let replaceStrArray = {
+      YYYY: year,
+      Y: year,
+      MM: ("0" + month).slice(-2),
+      M: month,
+      DD: ("0" + day).slice(-2),
+      D: day,
+      WW: weekday,
+      hh: ("0" + hours).slice(-2),
+      h: hours,
+      mm: ("0" + minutes).slice(-2),
+      m: minutes,
+      ss: ("0" + secounds).slice(-2),
+      s: secounds,
+      AP: ampm
+    };
+
+    let replaceStr = "(" + Object.keys(replaceStrArray).join("|") + ")";
+    let regex = new RegExp(replaceStr, "g");
+
+    ret = format.replace(regex, function(str) {
+      return replaceStrArray[str];
+    });
+
+    return ret;
+  };
 };
